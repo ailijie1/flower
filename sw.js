a3d7d9766eb4f471747d13cfee9276beac60839c
ailijie1 <1135726483@qq.com>
2026-05-01 17:43:13 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,28 @@
 
+const CACHE_NAME = 'flower-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/site.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).then(res => {
+        if (res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return res;
+      });
+    })
+  );
+});
